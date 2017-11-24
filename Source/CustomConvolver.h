1501dff876881ad083cc4e290c2741a869432216
Jack Campbell <jackcampbell@acm.org>
2017-11-23 18:25:46 -0600

    adding members to custom convolver

--- Source/CustomConvolver.h
+++ Source/CustomConvolver.h
@@ -23,4 +23,17 @@ public:
     void setImpulseResponse(File ir) override final;
     void process(dsp::AudioBlock<float> block) override final;
     void reset() override final;
+private:
+    //
+    double _sampleRate;
+    int _samplesPerBlock; // L
+    int _fftSize; // 2 * L; also subfilter size
+    int _numSubfilters; // (IR length) / L
+    
+    // array of IR FFT partitions
+    Array<dsp::FFT> _subfilters;
+    
+    // OLAP buffer
+    dsp::AudioBlock<float> _olap;
+    
 };
